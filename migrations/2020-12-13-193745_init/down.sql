bf2bc2973c022ba3981ab4d202bf902debb7572c
elhem <43937725+Elhemist@users.noreply.github.com>
2020-12-14 00:26:37 +0300

    Add rust base

---
+++ migrations/2020-12-13-193745_init/down.sql
@@ -0,0 +1,16 @@
+DROP TABLE products_in_orders;
+DROP TABLE orders;
+DROP TYPE order_status;
+
+DROP TABLE addresses;
+DROP TABLE products;
+DROP TABLE product_category;
+
+DROP TABLE courier;
+DROP TYPE courier_status;
+
+DROP TABLE transport;
+DROP TYPE transport_colors;
+
+DROP TABLE users;
+DROP TYPE user_role;
